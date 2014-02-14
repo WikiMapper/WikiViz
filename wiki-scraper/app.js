0c0dfe69ef0e8e1183c12a71eef2aa4bee1f55ec
Farhad Ghayour <me@FarhadG.com>
2014-02-14 12:43:59 -0800

    basic changes

--- wiki-scraper/app.js
+++ wiki-scraper/app.js
@@ -1,42 +1,40 @@
-var fs        = require('fs');
-var $         = require('cheerio');
-var request   = require('request');
-var startTime = new Date().getTime();
+var fs           = require('fs');
+var $            = require('cheerio');
+var request      = require('request');
+var startTime    = new Date().getTime();
+var childProcess = require('child_process');
 
-var dirLinks  = ['http://en.wikipedia.org/wiki/Special:AllPages'];
-var pageLinks = [];
+var website    = 'http://en.wikipedia.org/';
+var dirLinks   = ['wiki/Special:AllPages'], pageLinks = [];
+var dirCounter = 0;
 
-var writeToTemp = function() {
-  fs.appendFile(__dirname + '/pages/tempPageLinks.txt', pageLinks);
-  fs.appendFile(__dirname + '/dirs/tempDirLinks.txt', dirLinks);
+var writeToFile = function() {
+  fs.appendFile(__dirname + '/pages/pageLinks.txt', pageLinks);
+  fs.appendFile(__dirname + '/dirs/dirLinks.txt', dirLinks);
+  dirLinks = [], pageLinks = [];
 };
 
 var pushPageLinks = function(html) {
   html('a.mw-redirect').map(function(i, link) {
     var href = $(link).attr('href');
-    pageLinks.push('http://www.wikipedia.org' + href);
+    pageLinks.push(href);
   });
 };
 
 var getNextPage = function(html) {
-  var nextPageURL = 'http://en.wikipedia.org' + html('a[title="Special:AllPages"]').last().attr('href');
+  var nextPageURL = html('a[title="Special:AllPages"]').last().attr('href');
   dirLinks.push(nextPageURL);
-  request(nextPageURL, getHTML);
+  request(website+nextPageURL, getHTML);
 };
-
-var writeToMaster = function() {
-  fs.writeFile(__dirname + '/pages/pageLinks.txt', pageLinks);
-  fs.writeFile(__dirname + '/dirs/dirLinks.txt', dirLinks);
-};
-  
 
 var getHTML = function(err, resp, html) {
-  var parsedHTML = $.load(html);
-
-  if (dirLinks.length % 500 === 0) {
-    writeToTemp();
+  if (dirLinks.length % 10 === 0) {
+    console.log("\n #### SCRAPPIN ####");
+    console.log(dirLinks.length * ++dirCounter, "dirs brawled!!!");
+    writeToFile();
   }
 
+  var parsedHTML = $.load(html);
   pushPageLinks(parsedHTML);
 
   var nextPageExists = !!parsedHTML('a[title="Special:AllPages"]').last().text().match(/Next Page/gi);
@@ -44,7 +42,6 @@ var getHTML = function(err, resp, html) {
   if (nextPageExists) {
     getNextPage(parsedHTML);
   } else {
-    writeToMaster();
     var endTime = new Date().getTime();
     console.log("\n ########### WE'RE SCRAPPIN', BRO!!! ########### \n",
                 "Street Scrapper brawled up some fools in", 
@@ -53,4 +50,43 @@ var getHTML = function(err, resp, html) {
   }
 };
 
-request(dirLinks[0], getHTML);
+request(website+dirLinks[0], getHTML);
+
+
+var child = childProcess.fork(__dirname + "/twitter.js", keywordData);
+  child.on("exit", function(){
+    console.log(keywordData[0] + ": died :(");
+  });
+  child.on("message", function(text) {
+    console.log(keywordData[0] + ": " + text);
+  });
+  children.push(child);
+});
+
+process.on("exit", function() {
+  children.forEach(function(child){
+    child.kill();
+  });
+});
+
+
+// var cluster = require('cluster');
+// var http = require('http');
+// var numCPUs = require('os').cpus().length;
+
+// if (cluster.isMaster) {
+//   // Fork workers.
+//   for (var i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+
+//   cluster.on('death', function(worker) {
+//     console.log('worker ' + worker.pid + ' died');
+//   });
+// } else {
+//   // Worker processes have a http server.
+//   http.Server(function(req, res) {
+//     res.writeHead(200);
+//     res.end("hello world\n");
+//   }).listen(8000);
+// }
