797f5e69a4bbd20f59325ad55fb8eced685bb7ac
Farhad Ghayour <me@FarhadG.com>
2014-02-15 12:35:35 -0800

    working with the ORM

--- dataServer/app.js
+++ dataServer/app.js
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////////////
-// Dependencies
+// DEPENDENCIES
 //////////////////////////////////////////////////////////////////
 
 var express   = require('express');
@@ -10,15 +10,53 @@ var path      = require('path');
 var Waterline = require('waterline');
 var _         = require('lodash');
 
-
-/********
-Instantiate a new instance of the express and ORM
-*********/
-
 var app = express();
+
+
+//////////////////////////////////////////////////////////////////
+// ORM CONFIG
+//////////////////////////////////////////////////////////////////
+
+// Instantiate an ORM instance, require the adapter and config
 var orm = new Waterline();
+var diskAdapter = require('sails-mysql');
+
+var config = {
+  adapter: {
+    default: mysqlAdapter,
+    mysql: mysqlAdapter
+  },
+
+  connections: {
+    myLocalSql: {
+      adapter: 'mysql',
+      host: 'localhost',
+      database: 'wikiUrls'
+    }
+  }
+};
 
 
+//////////////////////////////////////////////////////////////////
+// ORM MODELS
+//////////////////////////////////////////////////////////////////
+
+var Url = Waterline.Collection.extend({
+  identity: 'url',
+  connection: 'myLocalSql',
+
+  attributes: {
+    href: 'string'
+  }
+});
+
+// Load the Model into the ORM
+orm.loadCollection(Url);
+
+
+//////////////////////////////////////////////////////////////////
+// EXPRESS SETUP
+//////////////////////////////////////////////////////////////////
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
@@ -31,18 +69,60 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+// Build Express Routes (CRUD routes for /users)
+app.get('/urls', function(req, res) {
+  app.models.url.find().done(function(err, models) {
+    if(err) return res.join({ err: err }, 500);
+    res.join(models);
+  });
 });
 
+app.post('/urls', function(req, res) {
+  app.models.url.create(req.body, function(err, model) {
+    if(err) return res.join({ err: err }, 500);
+    res.join(model);
+  });
+});
 
+app.get('/urls/:id', function(req, res) {
+  app.models.url.findOne({ id: req.params.id }, function(err, model) {
+    if(err) return res.join({ err:err }, 500);
+    res.json(model);
+  });
+});
 
+app.del('/users/:id', function(req, res) {
+  app.models.user.destroy({ id: req.params.id }, function(err) {
+    if(err) res.join({ err:err }, 500);
+    res.json({ status: 'OK' });
+  });
+});
 
+app.put('/users/:id', function(req, res) {
+  // Don't pass ID to update
+  delete req.body.id;
 
+  app.models.user.update({ id: req.params.id }, req.body, function(err, model) {
+    if(err) res.join({ err:err }, 500);
+    res.json(model);
+  });
+});
+
+//////////////////////////////////////////////////////////////////
+// START THE ORM & SERVER
+//////////////////////////////////////////////////////////////////
+
+// Start the ORM passing adapter in
+orm.initialize(config, function(err, models) {
+  if(err) throw err;
+
+  app.models      = models.collections;
+  app.connections = models.connections;
+
+  // Start Server
+  http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+  });
+
+  // app.listen(3000);
+});
