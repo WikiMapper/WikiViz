d0c798fead044595abb63b22334e558f9c16882b
Farhad Ghayour <me@FarhadG.com>
2014-02-14 16:13:12 -0800

    TANK & ROCKY are alive

--- wiki-scraper/scrapeUrls.js
+++ wiki-scraper/scrapeUrls.js
@@ -1,11 +1,11 @@
-var fs           = require('fs');
-var $            = require('cheerio');
-var request      = require('request');
-var startTime    = new Date().getTime();
+var fs         = require('fs');
+var $          = require('cheerio');
+var request    = require('request');
+var startTime  = new Date().getTime();
 
-var name     = process.argv[2];
-var startURL = process.argv[3];
-var endURL   = process.argv[4];
+var name       = process.argv[2];
+var startURL   = process.argv[3];
+var endURL     = process.argv[4];
 
 var website    = 'http://en.wikipedia.org/';
 var dirLinks   = [startURL], pageLinks = [];
@@ -14,27 +14,40 @@ var dirCounter = 0;
 var writeToFile = function() {
   fs.appendFile(__dirname + '/links/pageLinks-'+ name +'.txt', pageLinks);
   fs.appendFile(__dirname + '/links/dirLinks-'+ name +'.txt', dirLinks);
-  dirLinks = [startURL], pageLinks = [];
+  dirLinks = [], pageLinks = [];
 };
 
 var pushPageLinks = function(html) {
   html('a.mw-redirect').map(function(i, link) {
     var href = $(link).attr('href');
-    pageLinks.push('\n'+href);
+    pageLinks.push(href);
   });
 };
 
-var getNextPage = function(html, nextPageURL) {
-  dirLinks.push('\n'+nextPageURL);
+var getNextPage = function(nextPageURL) {
+  dirLinks.push(nextPageURL);
   request(website+nextPageURL, getHTML);
 };
 
+var trollWiki = function() {
+  setTimeout(function() {
+    getNextPage(dirLinks.slice(-1));
+  }, 1000);
+};
+
 var getHTML = function(err, resp, html) {
+  if (err) console.log(err);
+
+  if (resp.statusCode !== 200) {
+    trollWiki();
+    return;
+  }
+
   var parsedHTML = $.load(html);
   pushPageLinks(parsedHTML);
-  writeToFile();
 
-  if (dirLinks.length % 100 === 0) {
+  if (dirLinks.length % 50 === 0) {
+    writeToFile();
     console.log("\n #### "+ name + " IS SCRAPPIN, BRO! ####");
     console.log(dirLinks.length * ++dirCounter, "dirs brawled!!!");
   }
@@ -45,8 +58,9 @@ var getHTML = function(err, resp, html) {
   var finishedPartition = (nextPageURL === endURL);
 
   if (nextPageExists && !finishedPartition) {
-    getNextPage(parsedHTML, nextPageURL);
+    getNextPage(nextPageURL);
   } else {
+    writeToFile();
     var endTime = new Date().getTime();
     console.log("\n ########### WE'RE SCRAPPIN', BRO!!! ########### \n",
                 "Street Scrapper brawled up some fools in", 
