3f4d9529ebca1766e96f461bb790f90fc7ebe3fa
Autumn Fjeld <nmutua@gmail.com>
2014-02-21 11:36:35 -0800

    Styling and new service for http request

--- wikiviz-client/app/scripts/services/database-service.js
+++ wikiviz-client/app/scripts/services/database-service.js
@@ -1,4 +1,29 @@
-{"nodes:[
+
+angular.module('VisApp')
+  .factory('DatabaseService', ['$http', function($http) {
+
+    // var doRequest = function() {
+    //   console.log('making request');
+    //   return $http({
+    //     method: 'JSONP',
+    //     url: '../data/testdata.json' 
+    //   })
+    //   .then(function(data){
+    //     console.log(data);
+    //     return data;
+    //   })
+    //   .catch(function(e){
+    //     throw e;
+    //   });
+    //   //return {'url': 'www.hi.com'};
+    // };
+
+    // return {
+    //   request : function(url) { return doRequest(url); }
+    // };
+
+    var data =
+    {"nodes":[
         {"title" : "Article One",    "url" : "www.wikepedia.com",    "hits" : 20, "group" : 1},
         {"title" : "Article Two",    "url" : "www.wikepedia.com",    "hits" : 10, "group" : 1},
         {"title" : "Article Three",  "url" : "www.wikepedia.com",  "hits" : 8, "group" : 1},
@@ -16,12 +41,29 @@
         {"title" : "Article Fifteen",  "url" : "www.wikepedia.com",    "hits" : 10, "group" : 1}
     ],
    "links" : [
-        {"source":0,"target":1,"value":100}, {"source":0,"target":2,"value":10},
+        {"source":0,"target":1,"value":100}, {"source":0,"target":2,"value":100},
         {"source":0,"target":3,"value":30},  {"source":0,"target":4,"value":100},
-        {"source":0,"target":5,"value":100}, {"source":0,"target":6,"value":20},
-        {"source":0,"target":11,"value":100}, {"source":0,"target":12,"value":100},
-        {"source":0,"target":7,"value":100}, {"source":2,"target":8,"value":10},
-        {"source":3,"target":10,"value":20}, {"source":4,"target":11,"value":10},
-        {"source":5,"target":9,"value":20}, {"source":6,"target":13,"value":50},
-        {"source":6,"target":14,"value":80},  {"source":7,"target":14,"value":100}
-    ]}
+        {"source":0,"target":5,"value":100}, {"source":0,"target":6,"value":100},
+        {"source":0,"target":7,"value":100}, {"source":2,"target":8,"value":100},
+        {"source":0,"target":10,"value":20}, {"source":4,"target":11,"value":100},
+        {"source":0,"target":9,"value":20}, {"source":6,"target":13,"value":100},
+        {"source":0,"target":12,"value":80},  {"source":7,"target":14,"value":100}
+    ]};
+
+    return { request : data };
+  }]);
+
+/**
+ * this service will :
+ * query table 1 in database for the url
+ * query table 2 to get parent & children
+ * put this data into array of objects
+ * query table 1 to get title, other info
+ * 
+ */
+
+//
+// Creating a service simply returns a function that returns an object. 
+// This object is created with the creation of the application instance 
+// (remember, it’s a singleton object).
+
