d827a1b565f43741eff83194482c4332ef40383d
Farhad Ghayour <me@FarhadG.com>
2014-02-25 10:52:14 -0800

    for Joseph

--- dataServer/scraper/scrape.js
+++ dataServer/scraper/scrape.js
@@ -3,33 +3,41 @@ var request = require('request');
 var db      = require('database/db');
 
 
+var insertDb = function(data) {
+  db.insertInputUrl(data, function() {
+    console.log('inserted to database');
+  });  
+};
+
+
+var getLinks = function(err, resp, html, url) {
+  var $ = cheerio.load(html);
+  var title = $('title').text();
+
+  title = title.replace(/ - Wikipedia, the free encyclopedia/, "");
+  url = url.replace(/http:\/\/en.wikipedia.org\/wiki/, "");
+
+  var links = [];
+  $('#mw-content-text a').each(function(i, link) {
+    var internal = /#/gi.test( $(link).attr('href') );
+    if (!internal) {
+      var href = $(link).attr('href');
+      links.push(href);
+    }
+  });
+
+  insertDb({
+    url: url,
+    title: title,
+    incoming: null,
+    outgoing: links.length
+  });
+};
+
 var scrape = function(url) {
-
-  var getLinks = function(err, resp, html) {
-    var $ = cheerio.load(html);
-    var title = $('title').text();
-
-    var links = [];
-
-    $('#mw-content-text a').each(function(i, link) {
-      var internal = /#/gi.test( $(link).attr('href') );
-      if (!internal) {
-        var href = $(link).attr('href');
-        links.push(href);
-      }
-    });
-
-    db.insertInputUrl({
-      url: url,
-      title: title,
-      incoming: null,
-      outgoing: links.length
-    }, function() {
-      console.log('inserted');
-    })    
-  };
-
-  request(url, getLinks);
+  request(url, function(err, resp, html) {
+    getLinks(err, resp, html, url);
+  });
 };
 
 
