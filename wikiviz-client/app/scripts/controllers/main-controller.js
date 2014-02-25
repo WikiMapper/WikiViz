48b6d886119996086a9259dd514253d82bca18a2
Autumn Fjeld <nmutua@gmail.com>
2014-02-24 19:23:55 -0800

    update client files

--- wikiviz-client/app/scripts/controllers/main-controller.js
+++ wikiviz-client/app/scripts/controllers/main-controller.js
@@ -1,27 +1,23 @@
 angular.module('VisApp')
   .controller('MainCtrl', ['DatabaseService', 'd3Service', '$scope', '$location',
     function(DatabaseService, d3Service, $scope, $location){
+      console.log('MainCtrl called. $scope.showForm');
 
-      $scope.greeting = "Hi! We're going to show you some data";
+      $scope.showForm = true;
+      $scope.showVis = false;
 
-      $scope.getInput = function(){
-        $scope.sourcedata = DatabaseService.request;
-        console.log('got info', $scope.somedata);
+      $scope.reset = function(){
+        console.log('should call right away');
+        $scope.showForm = true;
+        $scope.showVis = false;
+        $scope.url=null;
       };
 
-
-
-      //get json data and assign to global variable
-      // function testData(){
-      //   console.log('shold not see this');
-      //   d3Service.d3().then(function(d3){
-      //     d3.json("data/testdata.json", function(jsondata){
-      //       $scope.sourcedata = jsondata;
-      //       console.log('got data',$scope.sourcedata);
-      //       $scope.$apply();  //this makes sourcedata available to directive
-      //     });
-      //   });
-      // };
+      $scope.getInput = function(){
+        $scope.showForm = false;
+        $scope.showVis = true;
+        //DatabaseService.request($scope.url);
+        $scope.sourcedata = DatabaseService.request($scope.url);
+      };
 
   }]);
-
