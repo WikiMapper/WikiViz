18ccdb8318e39a0171bf81ad96b5e2e94bbbb1ef
redwoodfavorite <joseph.michael.sample@gmail.com>
2014-02-25 15:52:43 -0800

    added express to package.json

--- dataServer/app.js
+++ dataServer/app.js
@@ -7,8 +7,8 @@ var routes  = require('./routes');
 var user    = require('./routes/user');
 var http    = require('http');
 var path    = require('path');
-var scrape  = require('scraper/scrape').scrape;
-var db      = require('database/db');
+var scrape  = require('./scraper/scrape').scrape;
+var db      = require('./database/db');
 
 var app = express();
 
