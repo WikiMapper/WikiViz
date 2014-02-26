046237c0f16eb600a4f5edb97e36f9317672058c
Michael Tseng <matseng@gmail.com>
2014-02-26 15:34:00 -0800

    Post req will return array of link objects

--- app/scraper/scrape.js
+++ app/scraper/scrape.js
@@ -24,8 +24,12 @@ var getLinks = function(err, resp, html, url) {
   $('#mw-content-text a').each(function(i, link) {
     var internal = /#/gi.test( $(link).attr('href') );
     if (!internal) {
+      var linkTitle = $(link).attr('title'); 
       var href = $(link).attr('href');
-      links.push(href);
+      links.push({
+        title: linkTitle,
+        url: 'http://wikipedia.com'+ href
+      });
     }
   });
 
