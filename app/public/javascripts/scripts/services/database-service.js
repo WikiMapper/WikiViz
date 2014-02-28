8c2ac4501f5038f1fbf5cd8a2fa0e4999bac7622
redwoodfavorite <joseph.michael.sample@gmail.com>
2014-02-28 11:24:28 -0800

    changed d3 to iterate over number of children

--- app/public/javascripts/scripts/services/database-service.js
+++ app/public/javascripts/scripts/services/database-service.js
@@ -60,12 +60,12 @@ angular.module('VisApp')
         //should we limit number of child links??
         console.log('before adding new node-urls, d3data', d3data);
         //angular.forEach(children, function (item, i ){
-        var queryCount = 20;  //the number of nodes per query
+        var queryCount = children.length;  //the number of nodes per query
         for (var i = 0; i < queryCount; i++){
           item = children[i];
           count++;
           var rank = queryCount - i;
-      
+
           var childNode = {};
           childNode.id       = count;
           childNode.title    = item.title;
@@ -95,7 +95,7 @@ angular.module('VisApp')
     };
   }]);
 
-    
+
 
 
   //   var count = 0,
@@ -110,10 +110,10 @@ angular.module('VisApp')
   //       sourceid = url;
   //       startNode = nodes.length;
   //     }else{
-  //       sourceid = 0; 
+  //       sourceid = 0;
   //       startNode = 0;
   //     }
-       
+
 
   //     angular.forEach(_.range(startNode, startNode+url), function(value){
   //       var node = {};
@@ -138,7 +138,7 @@ angular.module('VisApp')
   //     count++;
   //     return dummydata;
   //   };
-    
+
   //   return {
   //     request : doRequest
   //   };
@@ -150,15 +150,15 @@ angular.module('VisApp')
  * query table 2 to get parent & children
  * put this data into array of objects
  * query table 1 to get title, other info
- * 
+ *
  */
 
 //
-// Creating a service simply returns a function that returns an object. 
-// This object is created with the creation of the application instance 
+// Creating a service simply returns a function that returns an object.
+// This object is created with the creation of the application instance
 // (remember, it’s a singleton object).
 // AhHa! it's putting the returned function 'out there' - able to be called at any time
-// returning a static value defeats the purpose.  
+// returning a static value defeats the purpose.
 
 //Extends the destination object dst by copying all of the properties from the src object(s) to dst. You can specify multiple src objects.
 
