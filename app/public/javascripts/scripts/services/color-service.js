407e0866ba56996f20eede777572e2b5b50b9de7
Autumn Fjeld <nmutua@gmail.com>
2014-02-28 19:10:24 -0800

    added more colors to colorservice

--- app/public/javascripts/scripts/services/color-service.js
+++ app/public/javascripts/scripts/services/color-service.js
@@ -6,14 +6,15 @@ angular.module('VisApp')
           1  : "darkblue",
           2  : "crimson",
           3  : "darkgreen",
-          4  : "darkorchid",
+          4  : "tomato",
           5  : "darkviolet",
           6  : "orange",
           7  : "gold",
           8  : "mediumslateblue",
           9  : "darkblue",
           10 : "seagreen",
-          11 : "red"
+          11 : "red",
+          12 : "darkorchid"
       };
       
       return colormap[num];
